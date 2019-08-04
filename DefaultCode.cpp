6304a1b8724d0bd95e541a614be3832560fc90eb
phumtutum <48832481+phumtutum@users.noreply.github.com>
2019-08-04 19:23:44 +0300

    Create DefaultCode.cpp

---
+++ DefaultCode.cpp
@@ -0,0 +1,20 @@
+//check check
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<fstream>
+#include<queue>
+#include<cstring>
+
+#define ll long long
+#define pb(x) push_back(x)
+
+using namespace std;
+
+const int NMAX = 0;
+
+
+int main()
+{
+
+}
