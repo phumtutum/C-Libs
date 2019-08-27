3a3ad1398993779f8d93e658dd77a33f74f1be88
phumtutum <48832481+phumtutum@users.noreply.github.com>
2019-08-27 18:08:49 +0300

    added pair<int,int>

--- MyDefaultCode.cpp
+++ MyDefaultCode.cpp
@@ -1,15 +1,19 @@
-//check check
+//check check check
 #include<iostream>
 #include<vector>
 #include<algorithm>
 #include<fstream>
 #include<queue>
 #include<cstring>
+#include<map>
+#include<iomanip>
 
 #define ll long long
 #define pb(x) push_back(x)
 
 using namespace std;
+
+typedef pair<int,int> ii;
 
 const int NMAX = 0;
 
